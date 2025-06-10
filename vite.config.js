16ed6a44e8138887998346d191cf7ea157b49231
emmanuel-jaimes <jaimes.emmanuel@gmail.com>
2025-06-09 22:13:20 -0500

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mywebsite/dist',
+  base: '/',
   plugins: [react(), tailwindcss()],
 
 })
