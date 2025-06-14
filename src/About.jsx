712cd798da70e3ffb0ec6ac5aa1c5c38c072ffbc
emmanuel-jaimes <jaimes.emmanuel@gmail.com>
2025-06-13 19:22:50 -0500

    styling updates

--- src/About.jsx
+++ src/About.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import RecentlyPlayed from './RecentlyPlayed';
 import Typewriter from './Typewriter';
 import Card from './Card';
+import { title } from 'framer-motion/client';
 
 const sampleSongs = [
   {
@@ -31,42 +32,100 @@ const sampleSongs = [
   },
 ];
 
+const techStack = [
+  {
+    // title: "Java",
+    albumArt: "/java.svg"
+  },
+  {
+  //  title: "JavaScript",
+   albumArt: "/javascript.svg"
+  },
+  {
+  //  title: "python",
+   albumArt: "/python.svg"
+  },
+  {
+  //  title: "C++",
+   albumArt: "/c.svg"
+  },
+  {
+    albumArt: "/csharp.svg"
+  },
+  {
+    albumArt: "/html.svg"
+  },
+  {
+    albumArt: "/css.svg"
+  },
+  {
+    albumArt: "/haskell.svg"
+  },
+]
+const frameworks = [
+  {
+    albumArt: "/react.svg"
+  },
+  {
+    albumArt: "/typescript.svg"
+  },
+  {
+    albumArt: "vite.svg"
+  },
+  {
+    albumArt: "/expo.svg"
+  },
+  {
+    albumArt: "/node-js.svg"
+  },
+  {
+    albumArt: "/tailwindcss.svg"
+  },
+  {
+    albumArt: "/firebase.svg"
+  },
+  {
+    albumArt: "/php.svg"
+  },
+  {
+    albumArt: "/qt.svg"
+  }
+
+]
+
 function About() {
   return (
     <div className="text-left text-3xl"> 
-      <Typewriter
-            size=""
-            words={[
-                "Howdy, I'm a...",
-              "Software Engineer",
-              "University of Iowa Graduate",
-              "Dallas Native",
-              "Craft Bartender",
-              "Music Enthusiast",
-              "Intramural Futsal Champion",
-            ]}
-        />
-        <div className="text-base px-4 py-6 space-y-6 text-left">
 
-            <div className="leading-relaxed">
+        <Card>
+            <p className='text-2xl font-bold'>Howdy</p>
+            <div className="leading-relaxed items-center justify-center">
                 <p>
                 Whether it's solving complex problems or crafting beautiful solutions, I bridge curiosity, precision, and a hunger to keep learning. 
                 <br />
                 Oh—and can make <i>pretty mean</i> margaritas too. 🍸
                 </p>
             </div>
-        </div>
+        </Card>
+
+        <Card>
+          <p className='text-2xl font-bold'>Current Tech Stack</p>
+          <div className='space-y-4 flex flex-col'>
+            <RecentlyPlayed songs={techStack} color={'gray'}/>
+          <p className='text-xl'>Frameworks</p>
+            <RecentlyPlayed songs={frameworks}/>
+          </div>
+        </Card>
 
         <Card title={"Theme Music"}>
-            <div className="space-y-4 flex flex-col">
-                <p className="font-semibold">
-                    Every project deserves a theme song. Here’s my frequently played.
-                </p>
-                <div className="w-full max-w-4xl items-center justify-center text-center">
-                    <RecentlyPlayed songs={sampleSongs} />
-                </div>
+            <div className='space-y-4 flex flex-col'>
+              <p className='font-semibold'>Something on my rotation</p>
+            </div>
+            <div className='items-center justify-center text-center overflow-scroll'>
+              <RecentlyPlayed songs={sampleSongs}/>
             </div>
         </Card>
+
     </div>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 function Card({ title, children }) {
   return (
-    <div className="flex justify-center items-center w-full px-4 py-8">
-      <div className="bg-black rounded-2xl shadow-xl p-6 w-full max-w-3xl">
+    <div className="flex justify-center items-center py-8 w-full">
+      <div className="bg-black rounded-2xl shadow-xl p-6 w-full">
         {title && (
           <h2 className="text-2xl md:text-3xl font-bold mb-4 text-white text-center md:text-left">
             {title}
           </h2>
         )}
-        <div className="text-white text-base md:text-lg leading-relaxed">
+        <div className="text-white text-base md:text-lg leading-relaxed w-3xl">
           {children}
         </div>
       </div>
